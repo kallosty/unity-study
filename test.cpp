a5b720afc064d44a5f5afe55e1cf2636a692c002
kallosty <cnuastro2014@gmail.com>
2024-11-16 18:42:24 +0900

    holy

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define PRESET ios::sync_with_stdio(false);
+
+int main()
+{
+    PRESET;
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
